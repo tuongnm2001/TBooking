90fab31086c1ae5ce744c4abe04c148f37828951
Tuong Nguyen <tuongnm2001@gmail.com>
2023-03-15 09:54:21 +0700

    init project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+const App = () => {
+  const count = useSelector(state => state.counter.count);
+  const dispatch = useDispatch();
+
+  return (
+    <div className="App">
+      Hello
+    </div>
+  );
+}
+
+export default App;
