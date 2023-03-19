ff5e437694d970fe74a3193fa687779366bc556f
Tuong Nguyen <tuongnm2001@gmail.com>
2023-03-19 09:23:37 +0700

    design form specialty

--- src/Components/Home/HomePage.js
+++ src/Components/Home/HomePage.js
@@ -1,11 +1,13 @@
 import Banner from "./ContentHompage/Banner";
 import Doctor from "./ContentHompage/Doctor";
+import Specialty from "./ContentHompage/Specialty";
 
 
 const HomePage = (props) => {
     return (
         <div className="homepage-container">
             <Banner />
+            <Specialty />
             <Doctor />
             <div style={{ height: '300px' }}></div>
         </div>
