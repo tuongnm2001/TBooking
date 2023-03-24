7a4f8e2839e63bd77a557f1844edeccd80a52e76
Tuong Nguyen <tuongnm2001@gmail.com>
2023-03-24 10:05:46 +0700

    design extra doctor infor

--- src/Components/Admin/Content/ManageDoctor.js
+++ src/Components/Admin/Content/ManageDoctor.js
@@ -4,19 +4,35 @@ import MdEditor from 'react-markdown-editor-lite';
 import 'react-markdown-editor-lite/lib/index.css';
 import { useEffect, useState } from 'react';
 import Select from 'react-select';
-import { getAllDoctors, getDetailInforDoctor, saveDetailDoctor } from '../../../service/userService';
+import { fetchAllCode, getAllDoctors, getDetailInforDoctor, saveDetailDoctor } from '../../../service/userService';
 import { toast } from 'react-toastify';
+import Form from 'react-bootstrap/Form';
 
 const ManageDoctor = () => {
 
     const [contentMarkDown, setContentMarkDown] = useState('')
     const [contentHTML, setContentHTML] = useState('')
     const mdParser = new MarkdownIt(/* Markdown-it options */);
-    const [selectedDoctor, setselectedDoctor] = useState(null)
     const [description, setDescription] = useState('')
-    const [allDoctors, setAllDoctors] = useState({})
-    const [hasOldData, setHasOldData] = useState(false)
     const [loadingApi, setLoadingApi] = useState(false)
+    const [hasOldData, setHasOldData] = useState(false)
+
+    const [allDoctors, setAllDoctors] = useState({})
+    const [selectedDoctor, setSelectedDoctor] = useState(null)
+
+    //doctor infor table
+    const [listPrice, setListPrice] = useState({})
+    const [selectedPrice, setSelectedPrice] = useState(null)
+
+    const [listPayment, setListPayment] = useState({})
+    const [selectedPayment, setSelectedPayment] = useState(null)
+
+    const [listProvince, setListProvince] = useState({})
+    const [selectedProvince, setSelectedProvince] = useState(null)
+
+    const [nameClinic, setNameClinic] = useState('')
+    const [addressClinic, setAddressClinic] = useState('')
+    const [note, setNote] = useState('')
 
     const handleEditorChange = ({ html, text }) => {
         setContentMarkDown(text)
@@ -45,7 +61,7 @@ const ManageDoctor = () => {
     }
 
     const handleChangeSelect = async (selectedDoctor) => {
-        setselectedDoctor(selectedDoctor)
+        setSelectedDoctor(selectedDoctor)
         let res = await getDetailInforDoctor(selectedDoctor.value)
         if (res && res.errCode === 0 && res.data && res.data.Markdown) {
             let markdown = res.data.Markdown
@@ -77,6 +93,50 @@ const ManageDoctor = () => {
         }
     }
 
+    // const getRequiredDoctorInfor = async () => {
+    //     let resPrice = await fetchAllCode('PRICE');
+    //     let resPayment = await fetchAllCode('PAYMENT');
+    //     let resProvince = await fetchAllCode('PROVINCE');
+
+    //     if (resPrice && resPrice.errCode === 0 &&
+    //         resPayment && resPayment.errCode === 0 &&
+    //         resProvince && resProvince.errCode === 0 &&
+    //     ) {
+    //         let data = {
+    //             resPrice: resPrice.data,
+    //             resPayment: resPayment.data,
+    //             resProvince: resProvince.data
+    //         }
+    //     }
+    // }
+
+    useEffect(() => {
+        fetAllCodePrice()
+        fetAllCodePayment()
+        fetAllCodeProvince()
+    }, [])
+
+    const fetAllCodePrice = async () => {
+        let res = await fetchAllCode('PRICE')
+        if (res.errCode === 0) {
+            setListPrice(res.data)
+        }
+    }
+
+    const fetAllCodePayment = async () => {
+        let res = await fetchAllCode('PAYMENT')
+        if (res.errCode === 0) {
+            setListPayment(res.data)
+        }
+    }
+
+    const fetAllCodeProvince = async () => {
+        let res = await fetchAllCode('PROVINCE')
+        if (res.errCode === 0) {
+            setListProvince(res.data)
+        }
+    }
+
     const buidDataSelect = (inputData) => {
         let result = [];
         if (inputData && inputData.length > 0) {
@@ -103,6 +163,7 @@ const ManageDoctor = () => {
                 <div className='content-left'>
                     <label>Chọn bác sĩ</label>
                     <Select
+                        placeholder='Chọn bác sĩ'
                         className='select'
                         value={selectedDoctor}
                         onChange={handleChangeSelect}
@@ -120,6 +181,79 @@ const ManageDoctor = () => {
                     >
                         a
                     </textarea>
+                </div>
+            </div>
+
+            <div className='more-infor-extra row'>
+                <div className='col-4 form-group'>
+                    {/* <label>Chọn giá</label>
+                    <Select
+                        placeholder='Chọn giá'
+                        className='select'
+                        // value={selectedDoctor}
+                        // onChange={handleChangeSelect}
+                        options={listPrice}
+                    /> */}
+                    <Form.Label>Chọn giá</Form.Label>
+                    <Form.Select >
+                        {
+                            listPrice && listPrice.length > 0 &&
+                            listPrice.map((item, index) => {
+                                return (
+                                    <option key={`gender-${index}`} value={item.keyMap} >
+                                        {item.valueVi}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Form.Select>
+                </div>
+
+                <div className='col-4 form-group'>
+                    <Form.Label>Phương thức thanh toán</Form.Label>
+                    <Form.Select >
+                        {
+                            listPayment && listPayment.length > 0 &&
+                            listPayment.map((item, index) => {
+                                return (
+                                    <option key={`gender-${index}`} value={item.keyMap} >
+                                        {item.valueVi}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Form.Select>
+                </div>
+
+                <div className='col-4 form-group'>
+                    <Form.Label>Chọn tỉnh thành</Form.Label>
+                    <Form.Select >
+                        {
+                            listProvince && listProvince.length > 0 &&
+                            listProvince.map((item, index) => {
+                                return (
+                                    <option key={`gender-${index}`} value={item.keyMap} >
+                                        {item.valueVi}
+                                    </option>
+                                )
+                            })
+                        }
+                    </Form.Select>
+                </div>
+
+                <div className='col-4 form-group'>
+                    <label>Tên phòng khám</label>
+                    <input className='form-control' />
+                </div>
+
+                <div className='col-4 form-group'>
+                    <label>Địa chỉ phòng khám</label>
+                    <input className='form-control' />
+                </div>
+
+                <div className='col-4 form-group'>
+                    <label>Lưu ý</label>
+                    <input className='form-control' />
                 </div>
             </div>
 
@@ -141,7 +275,7 @@ const ManageDoctor = () => {
                     {
                         loadingApi &&
                         <i disabled={loadingApi} className="fa-solid fa-circle-notch fa-spin"></i>
-                    } {hasOldData === true ? 'Update' : 'Save'}
+                    } {hasOldData === true ? 'Sửa' : 'Lưu'}
                 </button>
             </div>
         </div>
